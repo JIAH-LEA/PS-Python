01d4bda3d4d7a7aba7ca3bd3b1aa98a025224a1f
JIAH LEE <66239292+JIAH-LEA@users.noreply.github.com>
2021-11-21 21:03:57 +0900

    H-Index.py (정렬)

---
+++ PS/Programmers/H-Index.py
@@ -0,0 +1,9 @@
+#Sort(정렬) 
+
+def solution(citations):
+    citations.sort()
+    answer = len(citations)
+    for i in range(answer):
+        if citations[i] >= answer-i:
+            return answer-i
+    return 0
